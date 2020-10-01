02f6daab4b49d43c3ad1fdbcde5b21b3e9a76662
Bea Graboloza <beagraboloza@gmail.com>
2020-09-30 21:58:05 -0300

    terminado index search

--- indexSearch.py
+++ indexSearch.py
@@ -8,18 +8,18 @@ B = 22
 
 
 def Bin(i, arr, index, indexf):
-    temp_i = (indexf+index)//2
-    if (indexf-index)!= 1:
+    if (indexf-index) > 0:
+        temp_i = (indexf+index)//2 + 1
         temp = arr[temp_i]
         if i > temp:
-            Bin(i, arr, temp_i, indexf) #No va temp_i + 1 porque temp_i sigue siendo un candidato
+            return Bin(i, arr, temp_i, indexf) #No va temp_i + 1 porque temp_i sigue siendo un candidato
         else:
             if i < temp:
-                Bin(i, arr, index, temp_i - 1) #Si va el -1 pq temp_i ya no es candidato
+                return Bin(i, arr, index, temp_i - 1) #Si va el -1 pq temp_i ya no es candidato
             else:
                 return temp_i
     else:
-        return temp_i
+        return index
         
 if __name__ == "__main__":
     S = []
@@ -34,9 +34,9 @@ if __name__ == "__main__":
         p = Pf.readline()
         if p =="":
             break
-        index = Bin(p, S, 0, len(S)-1) * B//11  #ayuda pq temp_i es none
-        for i in range(index, index+B//11):
-            if linecache.getline(sys.argv[1], i + 1) == p:
+        index = Bin(p, S, 0, len(S)-1) #ayuda pq temp_i es none
+        for i in range(index*B//11, (index+1)*B//11):
+            if linecache.getline(sys.argv[2], i + 1) == p:
                 output.append(p)
     Pf.close()
 
