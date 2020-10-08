a7b924580981b9aec999783e574880b4508a4a88
Bea Graboloza <beagraboloza@gmail.com>
2020-10-08 20:50:37 -0300

    intento de script para correr los cosos

--- scripttodos.py
+++ scripttodos.py
@@ -6,15 +6,16 @@ import binarySearch
 import linearSearch
 import indexSearch
 if __name__ == "__main__":
-    P = sys.argv[1]
-    T = sys.argv[2]
-    k_i = int(sys.argv[3])
-    k_f = int(sys.argv[4])
-    k_step = int(sys.argv[5])
-    for k in range(k_i,k_f+k_step,k_step):
-        for k2 in range(k):
-        print("generando")
-        os.system("python generator.py 10000 1000000")
-        print("buscando")
-        os.system("python binarySearch.py P.txt T.txt " + str(k) + ' ' + str(i))
-        print("terminado ciclo")
+    sizeT = sys.argv[1]
+    sizeP = [sizeT//4, sizeT//2, sizeT//6]
+    #  |P| < |T|/3     |P| > |T|/3     |P| > |T|/7
+    k = 15
+    for i in range(3):
+        for j in range(k):
+            print("generando")
+            os.system("python generator.py " + str(sizeP[i]) + ' ' + str(sizeT)) 
+            print("buscando")
+            os.system("python binarySearch.py P.txt T.txt " + str(i))
+            os.system("python indexSearch.py P.txt T.txt " + str(i))
+            os.system("python linearSearch.py P.txt T.txt " + str(i))
+            print("terminado ciclo")
