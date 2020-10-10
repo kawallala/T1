2ccc3cec69e9ce1514be61f45ecbf12bcb38e197
Martin <martin.araya.zavala@gmail.com>
2020-10-10 16:15:09 -0300

    realizados experimentos

--- scripttodos.py
+++ scripttodos.py
@@ -6,7 +6,7 @@ import binarySearch
 import linearSearch
 import indexSearch
 if __name__ == "__main__":
-    sizeT = sys.argv[1]
+    sizeT = int(sys.argv[1])
     sizeP = [sizeT//4, sizeT//2, sizeT//6]
     #  |P| < |T|/3     |P| > |T|/3     |P| > |T|/7
     k = 15
@@ -14,8 +14,10 @@ if __name__ == "__main__":
         for j in range(k):
             print("generando")
             os.system("python generator.py " + str(sizeP[i]) + ' ' + str(sizeT)) 
-            print("buscando")
+            print("buscando Binary")
             os.system("python binarySearch.py P.txt T.txt " + str(i))
+            print("Buscando Index")
             os.system("python indexSearch.py P.txt T.txt " + str(i))
+            print("Buscando Linear")
             os.system("python linearSearch.py P.txt T.txt " + str(i))
             print("terminado ciclo")
